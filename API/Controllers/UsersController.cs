3fb6b231ef989b4294532e4828bb41a192a8adec
Luis Alvarez <Luis.F.Alvarez@co.ey.com>
2021-09-28 17:26:11 -0500

    Version 4.0

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,10 +11,7 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-
-    public class UsersController : ControllerBase 
+    public class UsersController : BaseApiController  
     {
         private readonly DataContext _context;
 
@@ -22,15 +21,17 @@ namespace API.Controllers
         }
 
         [HttpGet]
-
+        [EnableCors("AllowOrigin")]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
         //api/users/3
+        [Authorize]
         [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUsers(int id)
-            
+       // [EnableCors("AllowOrigin")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)   
         {
             return await _context.Users.FindAsync(id);
         }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,12 +1,18 @@
 
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace API
 {
@@ -21,33 +27,35 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+            services.AddAppplicationServices(_config);
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+           
+            services.AddCors(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
             });
+            services.AddCors();
+            services.AddIdentityServices(_config);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseHsts();
-            app.UseHttpsRedirection(); 
+            app.UseHsts(); 
             app.UseRouting();
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+               
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:44366"));
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
